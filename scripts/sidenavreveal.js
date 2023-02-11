6293ad5fa20c53099ddc5cf1bf6de91367e9ca16
highrespasta <nv.schutz@gmail.com>
2023-02-11 21:17:33 +0100

    adding js to sidenav but not working yet

---
+++ scripts/sidenavreveal.js
@@ -0,0 +1,15 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".side-nav");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 1000;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
