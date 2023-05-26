72266633baee2d78e4a6842e31c9559c04d46bef
highrespasta <nv.schutz@gmail.com>
2023-05-26 21:16:56 +0200

    improved perspective and scale

--- scripts/randomfloatertrans.js
+++ scripts/randomfloatertrans.js
@@ -5,13 +5,16 @@ console.log(cards.length);
 console.log(cards);
 for (let i = 0; i < cards.length; i++) { 
     console.log(2);
-    cards[i].style.backgroundColor = "rgb("+randomcolor()+","+randomcolor()+","+randomcolor()+")";
-    cards[i].style.transform = "translate3d("+randomtransform()*2+"vmin,"+randomtransform()+"vmin,"+randomtransform()+"vmin)";
+    cards[i].firstChild.style.backgroundColor = "rgb("+randomcolor()+","+randomcolor()+","+randomcolor()+")";
+    cards[i].style.transform = "translate3d(calc("+randomtransform()+"vw - 50%),calc("+randomtransform()+"vh - 50%),"+randomtransformpos()+"px)";
 }
 function randomcolor() {
     return Math.floor(Math.random() * 255);
 }   
 function randomtransform(){
-    return Math.ceil(Math.random() * 30) * (Math.round(Math.random()) ? 1 : -1)
+    return Math.ceil(Math.random() * 20) * (Math.round(Math.random()) ? 1 : -1);
+}
+function randomtransformpos(){
+    return Math.ceil(Math.random() * 150);
 }
 }
