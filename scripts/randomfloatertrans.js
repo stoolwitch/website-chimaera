36300709dd4d08092e104a54ce7cf937e30bae89
highrespasta <nv.schutz@gmail.com>
2023-10-10 11:34:02 +0200

    prettier formatting in js

--- scripts/randomfloatertrans.js
+++ scripts/randomfloatertrans.js
@@ -1,20 +1,20 @@
 function assignrandom() {
-const cards = document.getElementsByClassName("floating-cards");
-console.log(randomcolor());
-console.log(cards.length);
-console.log(cards);
-for (let i = 0; i < cards.length; i++) { 
-    console.log(2);
-    cards[i].firstChild.style.backgroundColor = "rgb("+randomcolor()+","+randomcolor()+","+randomcolor()+")";
-    cards[i].style.transform = "translate3d(calc("+randomtransform()+"vw - 50%),calc("+randomtransform()+"vh - 50%),"+randomtransformpos()+"px)";
-}
-function randomcolor() {
-    return Math.floor(Math.random() * 255);
-}   
-function randomtransform(){
-    return Math.ceil(Math.random() * 20) * (Math.round(Math.random()) ? 1 : -1);
-}
-function randomtransformpos(){
-    return Math.ceil(Math.random() * 150);
-}
+    const cards = document.getElementsByClassName("floating-cards");
+    console.log(randomcolor());
+    console.log(cards.length);
+    console.log(cards);
+    for (let i = 0; i < cards.length; i++) {
+        console.log(2);
+        cards[i].firstChild.style.backgroundColor = "rgb(" + randomcolor() + "," + randomcolor() + "," + randomcolor() + ")";
+        cards[i].style.transform = "translate3d(calc(" + randomtransform() + "vw - 50%),calc(" + randomtransform() + "vh - 50%)," + randomtransformpos() + "px)";
+    }
+    function randomcolor() {
+        return Math.floor(Math.random() * 255);
+    }
+    function randomtransform() {
+        return Math.ceil(Math.random() * 20) * (Math.round(Math.random()) ? 1 : -1);
+    }
+    function randomtransformpos() {
+        return Math.ceil(Math.random() * 150);
+    }
 }
